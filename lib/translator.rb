7547313585e87efc4ab2df90bc2ba97df57001ee
fortuitous-counter-8979 <gmcharmy@gmail.com>
2019-07-08 04:14:52 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -1,11 +1,23 @@
-# require modules here
+require "yaml"
+#[{angel: ["O:)", "☜(⌒▽⌒)☞"]},{angry:[">:(","ヽ(ｏ`皿′ｏ)ﾉ"]}]
 
-def load_library
-  # code goes here
+def load_library(file_path)
+hash=YAML.load_file(file_path)
+new_hash={'get_meaning' => {}, 'get_emoticon' => {}}
+hash.each{|key, value| 
+new_hash['get_meaning'][value[1]]=key
+new_hash['get_emoticon'][value[0]]=value[1] }
+  return new_hash
 end
 
-def get_japanese_emoticon
-  # code goes here
+def get_japanese_emoticon(file_path, emoticon)
+ hash=load_library(file_path)
+ result=hash['get_emoticon'][emoticon]
+ if result
+   return result
+ else
+   return "Sorry, that emoticon was not found"
+ end
 end
 
 def get_english_meaning
